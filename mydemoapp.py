39791f3e497312ea9d3d5a5bde53f48a2e172d41
Ethan Vaughan <71590440+Ethanvon@users.noreply.github.com>
2023-03-15 18:13:09 +0000

    I joined github with replit!!!

--- mydemoapp.py
+++ mydemoapp.py
@@ -2,10 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello():
-    return "Hello! This is a basic Flask app. "
+  return "Hello! This is a basic Flask app. "
+
 
 if __name__ == "__main__":
-    app.run("0.0.0.0")
-  #this is a comment in python
+  app.run("0.0.0.0")
+#this is a comment in python
+#I joined github with replit!!!
+print("Hello Github!!")
